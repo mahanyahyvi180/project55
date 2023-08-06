04225edf110eadb2200cb1ac6737ce96c3393db4
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-06 22:57:07 +0330

    Add files via upload

---
+++ ConsoleApp55/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+
+            string name = Console.ReadLine();
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+    }
+}
